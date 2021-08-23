967f7c1c2c5053fbd3bf7d5081ead53675472adc
Nicholas Stoll <nicholasstoll@Nicholass-MacBook-Pro.local>
2021-08-23 14:18:39 -0400

    Updates

---
+++ precache-manifest.e535c1107de66861b476d1ba7c56aa71.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16e73029bd7d03857965dad147470ca",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a09abf33e7f9b636bb0b",
+    "url": "/react-portfolio/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "225dc2d356d66895cfb7",
+    "url": "/react-portfolio/static/css/main.66d216e9.chunk.css"
+  },
+  {
+    "revision": "a09abf33e7f9b636bb0b",
+    "url": "/react-portfolio/static/js/2.24892ded.chunk.js"
+  },
+  {
+    "revision": "225dc2d356d66895cfb7",
+    "url": "/react-portfolio/static/js/main.3d66041c.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "10aaf7bbcefbd50f7d1b514c28dd41cb",
+    "url": "/react-portfolio/static/media/colors.10aaf7bb.jpeg"
+  },
+  {
+    "revision": "41fe7190ff5aa585fb0102b37b860384",
+    "url": "/react-portfolio/static/media/devgrub.41fe7190.png"
+  },
+  {
+    "revision": "642a65a7335fcda7e76394e9680d0b12",
+    "url": "/react-portfolio/static/media/evverest.642a65a7.png"
+  },
+  {
+    "revision": "afeb6d5b9bad9d809a3e32aa87808203",
+    "url": "/react-portfolio/static/media/pomodoro.afeb6d5b.jpg"
+  },
+  {
+    "revision": "097e0250d11c7aeec3094dcb1e28a0d8",
+    "url": "/react-portfolio/static/media/van.097e0250.jpg"
+  },
+  {
+    "revision": "a60f956c115dfc0cce99805431c7b795",
+    "url": "/react-portfolio/static/media/youtube.a60f956c.png"
+  }
+]);
