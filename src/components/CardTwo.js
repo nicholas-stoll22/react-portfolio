96ea1c1b012ae21621f5174e229efaeeafbb61ff
Nicholas Stoll <nicholasstoll@Nicholass-MacBook-Pro.local>
2021-08-23 17:08:01 -0400

    Add existing file

--- src/components/CardTwo.js
+++ src/components/CardTwo.js
@@ -8,7 +8,7 @@ function CardTwo(props) {
     return(
         <div className="d-inline-block g-card" onClick={(e) => props.click(props.item)}>
             <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
-            { props.item.selected && <CardInfoTwo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
+            { props.item.selected && <CardInfoTwo title={props.item.title} subTitle={props.item.subTitle} /> }
         </div>
     );
 
