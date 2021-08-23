d134c9fbc98f13a8fff42b9408d458778a1ee26e
Nicholas Stoll <nicholasstoll@Nicholass-MacBook-Pro.local>
2021-08-23 01:53:24 -0400

    Add existing file

--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
